546a51eaf2f8fd385e5ab06ec9f5550fecc4d4a2
manuel360 <boamahemmanuel424@gmail.com>
2024-12-02 12:13:55 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,106 @@
+let main = document.querySelector('#main');
+let buttons= document.querySelectorAll('button')
+let display = document.querySelector('#display');
+let calc;
+let firstnum = ''
+let secnum = ''
+let operator = null;
+
+
+
+function show(value) {
+    display.innerHTML=value;
+
+        return display.innerHTML;
+}
+function clear() {
+display.innerHTML = '';
+firstnum =''
+secnum = ''
+operator = null;
+}
+buttons.forEach(btn =>{
+        btn.addEventListener('click', ()=>{
+                let inputEl = btn.getAttribute('data-value')
+                if (inputEl ==='C') {
+                        clear();
+                }
+                else if(!isNaN(inputEl)){
+                    if (operator===null) {
+                        firstnum+=inputEl;
+                        show(firstnum)
+                    }
+                        
+                    else {
+                        secnum+=inputEl;
+                        secnum +=firstnum
+                        show(secnum)
+                    }
+                }
+                else if(isNaN(inputEl)&&inputEl!=='='){
+                        operator = inputEl;
+                        show(inputEl)
+                     
+                }
+              
+                else if(inputEl ==='='){
+                  let result =  show(operate(parseFloat(firstnum), (operator), parseFloat(secnum)))
+                //  firstnum = result.toString();
+                //  secnum = ''
+                //  operator = null;
+                 console.log((`firstnum:${firstnum}, secnum${secnum}, operator${operator}`));
+                 
+                }
+              
+        })
+})
+
+
+
+function operate(first,operation,sec) {
+        if (operation ==='+') {
+             calc = add(first,sec)
+        }
+        else if (operation==='-') {
+                calc= subtract(first,sec)
+        }
+        else if (operation ==='/') {
+                calc = divide(first,sec)}
+        else if(operation ==='x'){
+                calc = multiply(first,sec)
+        }
+      
+        
+        return calc;
+        }
+        
+
+
+
+function add(a,b) {
+let addRes = a+b;
+return addRes;
+}
+
+function subtract(a,b) {
+    let subRes = a-b;
+    return subRes;
+    }
+    
+function divide(a,b) {
+        let divideRes = a/b;
+        if (b ==0) {
+               let message = 'error';
+                return message;
+        }
+        return divideRes
+}
+        
+function multiply(a,b) {
+        let multiRes = a*b;
+         return multiRes;
+         }
+     
+        
+
+  
